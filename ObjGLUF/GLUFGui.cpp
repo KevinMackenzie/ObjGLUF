6d2bf8d572f799e27ece142d6f2ff1f30c73c230
Kevin Mackenzie <amberstudioprogrammer@gmail.com>
2015-10-03 18:27:48 -0400

    Added License Information

--- ObjGLUF/GLUFGui.cpp
+++ ObjGLUF/GLUFGui.cpp
@@ -1,3 +1,21 @@
+/*
+
+Copyright (C) 2015  Kevin Mackenzie
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License http://www.gnu.org/licenses/gpl-3.0.en.html 
+for more details.
+
+*/
+
+
 #include "stdafx.h"
 #include "GLUFGui.h"
 //#include "CBFG/BitmapFontClass.h"
--- ObjGLUF/ObjGLUF.h
+++ ObjGLUF/ObjGLUF.h
@@ -1,9 +1,20 @@
-// The following ifdef block is the standard way of creating macros which make exporting 
-// from a DLL simpler. All files within this DLL are compiled with the OBJGLUF_EXPORTS
-// symbol defined on the command line. This symbol should not be defined on any project
-// that uses this DLL. This way any other project whose source files include this file see 
-// OBJGLUF_API functions as being imported from a DLL, whereas this DLL sees symbols
-// defined with this macro as being exported.
+/*
+
+Copyright (C) 2015  Kevin Mackenzie
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License http://www.gnu.org/licenses/gpl-3.0.en.html 
+for more details.
+
+*/
+
 #pragma once
 
 
